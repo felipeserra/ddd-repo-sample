f4aede9080520237d44c1aacb3bef2837162e771
daxnet <daxnet@outlook.com>
2018-01-19 20:17:07 +0800

    Added Dockerfile and swagger support.

--- src/RepositorySample.Service/Program.cs
+++ src/RepositorySample.Service/Program.cs
@@ -20,6 +20,7 @@ namespace RepositorySample.Service
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:9023")
                 .Build();
     }
 }
--- src/RepositorySample.Service/Startup.cs
+++ src/RepositorySample.Service/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,9 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
 using RepositorySample.Framework;
 using RepositorySample.Repositories.InMemory;
 using RepositorySample.Repositories.MongoDB;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace RepositorySample.Service
 {
@@ -27,6 +30,27 @@ namespace RepositorySample.Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Repository RESTful API",
+                    Version = "v1",
+                    Description = "仓储模式实现案例 RESTful API.",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Sunny Chen", Email = "daxnet@live.com", Url = "https://github.com/daxnet" },
+                    License = new License { Name = "Apache License 2.0", Url = "https://www.apache.org/licenses/LICENSE-2.0" }
+                });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xml = Path.Combine(basePath, "RepositorySample.Service.xml");
+                if (File.Exists(xml))
+                {
+                    c.IncludeXmlComments(xml);
+                }
+            });
 
             #region MongoDB 仓储实现
             //var mongoHost = Configuration["mongo:host"];
@@ -52,6 +76,13 @@ namespace RepositorySample.Service
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Repository RESTful API");
+            });
 
             app.UseMvc();
         }
